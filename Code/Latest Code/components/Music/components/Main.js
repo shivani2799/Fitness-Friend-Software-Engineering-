6866820d9a5bb0aa3c4490b9be529b5b268a4c0a
mmo91 <46290306+mmo91@users.noreply.github.com>
2020-05-07 22:21:01 -0400

    Update Main.js

--- Code/Latest Code/components/Music/components/Main.js
+++ Code/Latest Code/components/Music/components/Main.js
@@ -1,3 +1,6 @@
+// written by: Mya Odrick
+  // tested by: Mya Odrick
+  // debugged by: Mya Odrick
 import * as React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
